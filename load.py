fae2a6ef79a59f7ac499d503621635e8a22a3199
kostantin-kotochigov <konstantin.kotochigov@gmail.com>
2019-03-10 19:52:10 +0300

    fix

--- load.py
+++ load.py
@@ -1,4 +1,7 @@
 import pandas
+import pandas as pd
+import pyarrow.parquet as pq # Used to read the data
+import numpy as np
 import numpy
 from tqdm import tqdm
 from keras.models import Sequential, Model
@@ -37,58 +40,64 @@ def matthews_correlation(y_true, y_pred):
 data_dir = "/home/ubuntu/"
 temp_dir = "/home/ubuntu/vbs_temp/"
 
-train = pandas.read_parquet("train.parquet")
-test = pandas.read_parquet("test.parquet")
-meta = pandas.read_csv("metadata_train.csv")
-meta_test = pandas.read_csv("metadata_test.csv")
+df_train = pandas.read_csv(data_dir + 'metadata_train.csv')
+df_train = df_train.set_index(['id_measurement', 'phase'])
+df_train.head()
 
-n = int(train.shape[1]/3) # 2904
-N = train.shape[0] # 800000
+def min_max_transf(ts):
+     ts_std = (ts + abs(-128)) / (127 + abs(-128))
+     return (ts_std * (2) - 1)
 
-n_test = int(test.shape[1]/3) # 6679
-N_test = test.shape[0] # 800000
+sample_size = 800000
+def transform_ts(ts, chunk_size):
+    ts_std = min_max_transf(ts)
+    bucket_size = chunk_size
+    new_ts = []
+    for i in range(0, sample_size, bucket_size):
+        ts_range = ts_std[i:i + bucket_size]
+        mean = ts_range.mean()
+        std = ts_range.std() # standard deviation
+        std_top = mean + std # I have to test it more, but is is like a band
+        std_bot = mean - std
+        percentil_calc = np.percentile(ts_range, [0, 1, 25, 50, 75, 99, 100])
+        max_range = percentil_calc[-1] - percentil_calc[0] # this is the amplitude of the chunk
+        relative_percentile = percentil_calc - mean # maybe it could heap to understand the asymmetry
+        new_ts.append(np.concatenate([np.asarray([mean, std, std_top, std_bot, max_range]),percentil_calc, relative_percentile]))
+    return np.asarray(new_ts)
 
-X_raw, y_raw = [], []
-X_test_raw, y_test_raw = [], []
-for i in tqdm(range(n), total=n, unit="measurements"):
-    ind = i * 3
-    X_raw.append([train.iloc[:,ind], train.iloc[:,ind+1], train.iloc[:,ind+2]])
-    y_raw.append(meta['target'][ind])
+def prep_data(start, end, chunk_size):
+    praq_train = pq.read_pandas('train.parquet', columns=[str(i) for i in range(start, end)]).to_pandas()
+    X = []
+    y = []
+    for id_measurement in tqdm(df_train.index.levels[0].unique()[int(start/3):int(end/3)]):
+        X_signal = []
+        for phase in [0,1,2]:
+            signal_id, target = df_train.loc[id_measurement].loc[phase]
+            if phase == 0:
+                y.append(target)
+            X_signal.append(transform_ts(praq_train[str(signal_id)], chunk_size))
+        X_signal = np.concatenate(X_signal, axis=1)
+        X.append(X_signal)
+    X = np.asarray(X)
+    y = np.asarray(y)
+    return X, y
 
-for i in tqdm(range(n_test), total=n_test, unit="measurements"):
-    ind = i * 3
-    X_test_raw.append([test.iloc[:,ind], test.iloc[:,ind+1], test.iloc[:,ind+2]])
-
-def transform_signal(x):
-    signal_min = numpy.min(x)
-    signal_max = numpy.max(x)
-    signal_mean = numpy.mean(x)
-    signal_std = numpy.std(x)
-    std_top = signal_mean + signal_std
-    std_bot = signal_mean - signal_std
-    std_range = numpy.int16(signal_max) - signal_min
-    percentil_calc = np.percentile(x, [0, 1, 25, 50, 75, 99, 100])
-    max_range = percentil_calc[-1] - percentil_calc[0]
-    relative_percentile = percentil_calc - signal_mean
-    signal_features = numpy.concatenate([np.asarray([signal_min, signal_max, signal_mean, signal_std, std_top, std_bot, std_range, max_range]),percentil_calc, relative_percentile])
-    return (signal_features)
-
-def reduce_dimensionality(X, chunk_size=10000):
-    X_transformed = []
-    for i in tqdm(range(len(X)), total=len(X), unit="measurements"):
-        record_measurements = []
-        for chunk_num in range(int(len(X[0][0]) / chunk_size) - 1):
-            fr = chunk_num * chunk_size
-            to = (chunk_num + 1) * chunk_size
-            record_measurements.append(list(transform_signal(X[i][0][fr:to])) + list(transform_signal(X[i][1][fr:to])) +  list(transform_signal(X[i][0][fr:to])))
-        X_transformed.append(record_measurements)
-    return numpy.asarray(X_transformed)
+# this code is very simple, divide the total size of the df_train into two sets and process it
+def load_all(chunk_size=5000):
+    X = []
+    y = []
+    total_size = len(df_train)
+    for ini, end in [(0, int(total_size/2)), (int(total_size/2), total_size)]:
+        X_temp, y_temp = prep_data(ini, end, chunk_size)
+        X.append(X_temp)
+        y.append(y_temp)
+    X = np.concatenate(X)
+    y = np.concatenate(y)
+    return X,y
 
 
-# X, y = reduce_dimensionality(X_raw, y_raw, chunk_size = 10000)
 
-# reduced_dim = X.shape[1]
-# measurement_features = X.shape[2]
+
 
 class Attention(Layer):
     def __init__(self, step_dim,
@@ -145,8 +154,7 @@ class Attention(Layer):
 def model_lstm(input_shape):
     inp = Input(shape=(input_shape[1], input_shape[2],))
     x = Bidirectional(CuDNNLSTM(128, return_sequences=True))(inp)
-    x = Bidirectional(CuDNNLSTM(64, return_sequences=True))(x)
-    x = Attention(input_shape[1])(x)
+    x = Bidirectional(CuDNNLSTM(64, return_sequences=False))(x)
     x = Dense(64, activation="relu")(x)
     x = Dense(1, activation="sigmoid")(x)
     model = Model(inputs=inp, outputs=x)
@@ -173,25 +181,27 @@ def custom_fit(X, y, num_epochs, num_splits, batch_size=256):
     best_threshold = numpy.argmax(numpy.mean(stats, axis=0))
     return {"threshold":5*best_threshold, "mean":numpy.mean(stats, axis=0), "std":numpy.std(stats, axis=0)}
 
-# for chunk_size in [1000]:
-#     X = reduce_dimensionality(X_raw,chunk_size)
-#     numpy.save(temp_dir + "X_train_{}".format(chunk_size), X)
-#     X_test = reduce_dimensionality(X_test_raw, chunk_size)
-#     numpy.save(temp_dir + "X_test_{}".format(chunk_size), X_test)
+for chunk_size in [1000,3000,5000]:
+    X, y = load_all(chunk_size)
+    numpy.save(temp_dir + "X_train_{}".format(chunk_size), X)
+    numpy.save(temp_dir + "y_train_{}".format(chunk_size), y)
+    # X_test = reduce_dimensionality(X_test_raw, chunk_size)
+    # numpy.save(temp_dir + "X_test_{}".format(chunk_size), X_test)
 
 
 
 chunk_size = 5000
 X = numpy.load(temp_dir+"X_train_{}.npy".format(chunk_size))
-X_test = numpy.load(temp_dir+"X_test_{}.npy".format(chunk_size))
-y = numpy.asarray(y_raw)
+y = numpy.load(temp_dir+"y_train_{}.npy".format(chunk_size))
+# X_test = numpy.load(temp_dir+"X_test_{}.npy".format(chunk_size))
+# y = numpy.asarray(y_raw)
 # stats = custom_fit(X, y)
 
 result = []
-for num_epochs in [1, 2, 5, 10, 15, 20, 25, 75]:
-    stats = custom_fit(X, y, num_epochs=num_epochs, num_splits=2)
-    result.append((num_epochs, numpy.max(stats['mean'] - stats['std'])))
-    pandas.DataFrame(result, columns=['param','quality']).to_csv(data_dir+"grid_search.csv", sep=";", index=False)
+for num_epochs in [50]:
+    stats = custom_fit(X, y, num_epochs=num_epochs, num_splits=25)
+    result.append((chunk_size, numpy.max(stats['mean'] - stats['std']), 5 * numpy.argmax(stats['mean'] - stats['std']),numpy.max(stats['mean']), 5 * numpy.argmax(stats['mean'])))
+    pandas.DataFrame(result, columns=['param', 'lcb_quality', 'lcb_threshold', 'quality', 'threshold']).to_csv(data_dir + "grid_search.csv", sep=";", index=False)
 
 result = []
 num_epochs = 50
@@ -201,8 +211,8 @@ for chunk_size in [1000, 10000]:
     X_test = numpy.load(temp_dir + "X_test_{}.npy".format(chunk_size))
     y = numpy.asarray(y_raw)
     stats = custom_fit(X, y, num_epochs=num_epochs, num_splits=10, batch_size=128)
-    result.append((chunk_size, numpy.max(stats['mean'] - stats['std']), 5*numpy.argmax(stats['mean'] - stats['std'])))
-    pandas.DataFrame(result, columns=['param','quality','threshold']).to_csv(data_dir+"grid_search.csv", sep=";", index=False)
+    result.append((chunk_size, numpy.max(stats['mean'] - stats['std']), 5*numpy.argmax(stats['mean'] - stats['std']), numpy.max(stats['mean']), 5*numpy.argmax(stats['mean'])))
+    pandas.DataFrame(result, columns=['param','lcb_quality','lcb_threshold', 'quality','threshold']).to_csv(data_dir+"grid_search.csv", sep=";", index=False)
 
 result = []
 for model in models:
--- models.py
+++ models.py
@@ -36,8 +36,8 @@ def model_lstm_4(input_shape):
     x = Bidirectional(CuDNNLSTM(128, return_sequences=True))(inp)
     x = Bidirectional(CuDNNLSTM(64, return_sequences=True))(x)
     x = Attention(input_shape[1])(x)
-    x = Dense(64, activation="prelu")(x)
-    x = Dense(32, activation="prelu")(x)
+    x = Dense(64, activation="relu")(x)
+    x = Dense(32, activation="relu")(x)
     x = Dense(1, activation="sigmoid")(x)
     model = Model(inputs=inp, outputs=x)
     model.compile(loss='binary_crossentropy', optimizer='adam')
@@ -47,8 +47,8 @@ def model_lstm_5(input_shape):
     inp = Input(shape=(input_shape[1], input_shape[2],))
     x = Bidirectional(CuDNNLSTM(128, return_sequences=True))(inp)
     x = Attention(input_shape[1])(x)
-    x = Dense(64, activation="prelu")(x)
-    x = Dense(32, activation="prelu")(x)
+    x = Dense(64, activation="relu")(x)
+    x = Dense(32, activation="relu")(x)
     x = Dense(1, activation="sigmoid")(x)
     model = Model(inputs=inp, outputs=x)
     model.compile(loss='binary_crossentropy', optimizer='adam')
@@ -93,19 +93,29 @@ def model_lstm_9(input_shape):
     x = Bidirectional(CuDNNLSTM(8, return_sequences=True))(inp)
     x = Bidirectional(CuDNNLSTM(4, return_sequences=True))(x)
     x = Attention(input_shape[1])(x)
-    x = Dense(4, activation="prelu")(x)
-    x = Dense(2, activation="prelu")(x)
+    x = Dense(4, activation="relu")(x)
+    x = Dense(2, activation="relu")(x)
     x = Dense(1, activation="sigmoid")(x)
     model = Model(inputs=inp, outputs=x)
     model.compile(loss='binary_crossentropy', optimizer='adam')
     return model
 
-models = [model_lstm_1(), model_lstm_2(), model_lstm_3(), model_lstm_4(), model_lstm_5(), model_lstm_6()]
+# for chunk_size in [10000, 1000]:
+#     X, y = load_all(chunk_size)
+#     numpy.save(temp_dir + "X_train_{}".format(chunk_size), X)
+#     numpy.save(temp_dir + "y_train_{}".format(chunk_size), y)
+
+
+chunk_size = 5000
+inp_shape = X.shape
+models = [model_lstm_1(inp_shape), model_lstm_2(inp_shape), model_lstm_3(inp_shape), model_lstm_4(inp_shape), model_lstm_5(inp_shape), model_lstm_6(inp_shape), model_lstm_7(inp_shape), model_lstm_8(inp_shape), model_lstm_9(inp_shape)]
 
 result = []
-for model in models:
-    num_epochs = 25
-    stats = custom_fit(X, y, num_epochs=num_epochs, num_splits=10)
-    result.append((num_epochs, numpy.max(stats['mean'] - stats['std'])))
-    pandas.DataFrame(result, columns=['param','quality']).to_csv(data_dir+"grid_search.csv", sep=";", index=False)
+for model_num, model in enumerate(models):
+    num_epochs = 50
+    stats = custom_fit(X, y, num_epochs=num_epochs, num_splits=20)
+    model_result = ("model{}".format(model_num), numpy.max(stats['mean'] - stats['std']), 5 * numpy.argmax(stats['mean'] - stats['std']), numpy.max(stats['mean']), 5 * numpy.argmax(stats['mean']))
+    print(model_result)
+    result.append(model_result)
+    pandas.DataFrame(result, columns=['param', 'lcb_quality', 'lcb_threshold', 'quality', 'threshold']).to_csv(data_dir + "grid_search.csv", sep=";", index=False)
 
